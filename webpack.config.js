ce4b51745de7023b1b2f2e469d5a3c548b374706
Will Mainwaring <willm@yogainternational.com>
2019-09-18 18:35:56 -0400

    separate node modules into own bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,33 @@
 var path = require('path');
 var SRC_DIR = path.join(__dirname, '/client');
 var DIST_DIR = path.join(__dirname, '/public');
-
+var BrotliPlugin = require('brotli-webpack-plugin');
 // console.log(SRC_DIR);
 // DONT USE BABEL-LOADER 8, ONLY 7 || npm install -D babel-loader@7 babel-core babel-preset-env webpack
 
 module.exports = {
-  mode: 'none',
+  mode: 'production',
   entry: `${SRC_DIR}/index.jsx`,
+  devtool: '',
+  plugins: [
+    new BrotliPlugin({
+      asset: '[path].br[query]',
+      test: /\.jsx$/,
+      threshold: 10240,
+      minRation: 0.8
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  },
   output: {
     path: DIST_DIR,
     filename: 'bundle.js'
@@ -20,7 +40,7 @@ module.exports = {
         exclude: [/(node_modules)/, /(routes)/, /(models)/, /(server)/],
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'es2015'],
         }
       },
       {
